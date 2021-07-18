998e57bd0d93d808a1a497fceb82aa71111ae033
7papagreat <62813232+7papagreat@users.noreply.github.com>
2021-07-18 15:14:01 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(Hello());
+}
+
+class Hello extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'text',
+      home: Container(
+        color: Color(0xffEDEFF3),
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.fromLTRB(16, 48, 16, 16),
+              child: Container(
+                color: Color(0xffC4C3C4),
+                height: 110,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Container(
+                      color: Color(0xffC4C3C4),
+                      height: 110,
+                      width: 110,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Container(
+                      color: Color(0xffC4C3C4),
+                      height: 110,
+                      width: 110,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Container(
+                      color: Color(0xffC4C3C4),
+                      height: 110,
+                      width: 110,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Container(
+                color: Color(0xffC4C3C4),
+                height: 110,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Container(
+                color: Color(0xffC4C3C4),
+                height: 110,
+              ),
+            ),
+            Padding(
+                padding: EdgeInsets.all(0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.all(16.0),
+                          child: Container(
+                            color: Color(0xffC4C3C4),
+                            height: 110,
+                            width: 110,
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(16.0),
+                          child: Container(
+                            color: Color(0xffC4C3C4),
+                            height: 110,
+                            width: 110,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(0, 0, 16, 0),
+                      child: Container(
+                        color: Color(0xffC4C3C4),
+                        height: 252,
+                        width: 252,
+                      ),
+                    )
+                  ],
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+}
